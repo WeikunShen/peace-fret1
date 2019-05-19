9d99ecdc454aa57e64c3a8235a718a00d84fbca0
WeikunShen <42928279+WeikunShen@users.noreply.github.com>
2019-05-19 14:37:43 +0800

    Add files via upload

---
+++ vid5_multipleVidsMouseOver - 副本/README.md
@@ -0,0 +1,1 @@
+Fret/Peace/Fret/Peace
---
+++ vid5_multipleVidsMouseOver - 副本/sketch.js
@@ -0,0 +1,55 @@
+"use strict";
+
+
+let vid_0;
+let vid_0_playing = false;
+let vid_1;
+let vid_1_playing = false;
+let documentActive = false; // we need to make sure they engage with page before it can auto play
+
+
+function setup() {
+  // dophin vid 1!
+  vid_0 = createVideo(['assets/平静final.mp4']);;
+  vid_0.size(500, 400);
+
+  vid_0.mouseOver(playVid_0);
+  vid_0.mouseOut(pauseVid_0);
+
+  // dophin vid 2!
+  vid_1 = createVideo(['assets/烦躁final.mp4']);;
+  vid_1.size(500, 400);
+
+  vid_1.mouseOver(playVid_1);
+  vid_1.mouseOut(pauseVid_1);
+}
+
+function draw() {}
+
+function playVid_0() {
+  if (documentActive) {
+    vid_0.loop();
+    vid_0_playing = true;
+  }
+}
+
+function pauseVid_0() {
+  vid_0.pause();
+  vid_0_playing = false;
+}
+
+function playVid_1() {
+  if (documentActive) {
+    vid_1.loop();
+    vid_1_playing = true;
+  }
+}
+
+function pauseVid_1() {
+  vid_1.pause();
+  vid_1_playing = false;
+}
+
+function mousePressed() {
+  documentActive = true; // since they have clicked, now we can play
+}
